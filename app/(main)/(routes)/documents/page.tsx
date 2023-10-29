0969a7ca0ccaea208f3234f4aa0fbc20aef30add
zubayerhimel <zhimel007@gmail.com>
2023-10-29 23:04:47 +0600

    feat: added document page empty content

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import { useUser } from '@clerk/clerk-react';
+import { PlusCircle } from 'lucide-react';
+import Image from 'next/image';
+
+import { Button } from '@/components/ui/button';
+
 const Documents = () => {
-  return <h1>Documents</h1>;
+  const { user } = useUser();
+
+  return (
+    <div className='h-full flex flex-col justify-center items-center space-y-4'>
+      <Image src='/empty.png' alt='No content' height={300} width={300} className='dark:hidden' />
+      <Image src='/empty-dark.png' alt='No content' height={300} width={300} className='dark:block hidden' />
+      <h2 className='text-lg font-medium'>Welcome to {user?.firstName}&apos;s Zotion</h2>
+      <Button size='sm'>
+        <PlusCircle className='w-4 h-4 mr-2' /> Create new note
+      </Button>
+    </div>
+  );
 };
 
 export default Documents;
