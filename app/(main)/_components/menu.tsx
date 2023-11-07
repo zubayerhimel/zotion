918107cf43c7f71e0bbb96fc408707a1925d0ffa
zubayerhimel <zhimel007@gmail.com>
2023-11-07 21:52:09 +0600

    feat: added menu in page to delete the page

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { MoreHorizontal, Trash } from 'lucide-react';
+import { Skeleton } from '@/components/ui/skeleton';
+
+interface IMenu {
+  documentId: Id<'documents'>;
+}
+
+export const Menu = ({ documentId }: IMenu) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = () => {
+    const promise = archive({ id: documentId });
+    toast.promise(promise, {
+      loading: 'Moving to trash...',
+      success: 'Note moved to trash.',
+      error: 'Failed to archive note.',
+    });
+
+    router.push('/documents');
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size='sm' variant='ghost'>
+          <MoreHorizontal className='w-4 h-4' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-60' align='end' alignOffset={8} forceMount>
+        <DropdownMenuItem onClick={onArchive}>
+          <Trash className='w-4 h-4 mr-2' />
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className='text-xs text-muted-foreground p-2'>Last edited by {user?.fullName}</div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return <Skeleton className='h-6 w-10' />;
+};
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { Id } from '@/convex/_generated/dataModel';
 
 import Title from './title';
 import Banner from './banner';
+import { Menu } from './menu';
 
 interface INavbar {
   isCollapsed: boolean;
@@ -22,8 +23,11 @@ const Navbar = ({ isCollapsed, resetWidth }: INavbar) => {
 
   if (document === undefined) {
     return (
-      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center'>
+      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between'>
         <Title.Skeleton />
+        <div className='flex items-center gap-x-2'>
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -38,6 +42,9 @@ const Navbar = ({ isCollapsed, resetWidth }: INavbar) => {
         {isCollapsed && <MenuIcon role='button' onClick={resetWidth} className='h-6 w-6 text-muted-foreground' />}
         <div className='flex items-center justify-between w-full'>
           <Title initialData={document} />
+          <div className='flex items-center gap-x-2'>
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
 
