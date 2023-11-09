da99c24718046cdd6533f223abd9c0bf485b6536
zubayerhimel <zhimel007@gmail.com>
2023-11-09 21:27:44 +0600

    fix: trying to fix module not found error

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 
 import { SearchCommand } from '@/components/search-command';
 import { Spinner } from '@/components/spinner';
-import Navigation from './_components/navigation';
+import Navigation from '@/app/(main)/_components/navigation';
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
